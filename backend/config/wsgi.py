6711358dffa02081cc3fda58ae03f8eefd63bfd5
JOEL <kemtsopjoel85@gmail.com>
2026-05-24 11:19:47 +0100

    Corriger wsgi.py pour gunicorn (WSGI standard)

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -1,14 +1,6 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from apps.delivery.routing import websocket_urlpatterns
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(websocket_urlpatterns)
-    ),
-})
+application = get_wsgi_application()
